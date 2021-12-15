c6af98ff0dd4fe575bc6b0cd2fac0f15a4182464
Paula Forbes <paula.forbes@live.com.au>
2021-12-15 18:01:29 +1100

    updated files with current workings

---
+++ Wedding_JS.js
@@ -0,0 +1,19 @@
+function functionBubble() {
+    document.getElementById("myInviteBubble").style.backgroundImage = "url('bubble_background.jpeg')";
+}
+
+function functionLeaves() {
+    document.getElementById("myInviteLeaves").style.backgroundImage = "url('leaves_background.jpeg')";
+}
+
+function functionWater() {
+    document.getElementById("myInviteWater").style.backgroundImage = "url('watercolor_background.jpeg')";
+    imageWidth = "40px";
+    imageHeight = "40px";
+}
+
+function check1() {
+    let inputVal = document.getElementById("guestOne").value;
+    console.log(inputVal);
+}
+
