56aaace78fc1a15d470c93770faf52ad608c85f5
Paula Forbes <paula.forbes@live.com.au>
2021-12-17 16:50:14 +1100

    latest updates which includes all essential elements of the project. now finalising style and formatting.

--- Wedding_JS.js
+++ Wedding_JS.js
@@ -1,19 +1,45 @@
-function functionBubble() {
-    document.getElementById("myInviteBubble").style.backgroundImage = "url('bubble_background.jpeg')";
-}
-
-function functionLeaves() {
-    document.getElementById("myInviteLeaves").style.backgroundImage = "url('leaves_background.jpeg')";
-}
-
-function functionWater() {
-    document.getElementById("myInviteWater").style.backgroundImage = "url('watercolor_background.jpeg')";
-    imageWidth = "40px";
-    imageHeight = "40px";
-}
-
+//live typing functions
 function check1() {
-    let inputVal = document.getElementById("guestOne").value;
-    console.log(inputVal);
+    var x = document.getElementById("guestOne").value;
+    document.getElementById("first").innerHTML = x;
 }
 
+function check2() {
+    var x = document.getElementById("guestTwo").value;
+    document.getElementById("second").innerHTML = x;
+}
+
+function check3() {
+    var x = document.getElementById("weddingMessage").value;
+    document.getElementById("third").innerHTML = x;
+}
+
+function check4() {
+    var x = document.getElementById("time").value;
+    document.getElementById("fourth").innerHTML = x;
+}
+
+function check5() {
+    var x = document.getElementById("date").value;
+    document.getElementById("fifth").innerHTML = x;
+}
+
+function check6() {
+    var x = document.getElementById("location").value;
+    document.getElementById("sixth").innerHTML = x;
+}
+
+//background image function
+function changeBGImage(bgImage){
+
+    let element = document.getElementById('image');
+    if(bgImage == 'bubble'){
+        element.style.backgroundImage = "url('bubble_background.jpeg')";
+    }
+    if(bgImage == 'leaves'){
+        element.style.backgroundImage = "url('leaves_background.jpeg')";
+    }
+    if(bgImage == 'water color'){
+        element.style.backgroundImage = "url('watercolor_background.jpeg')";
+    }
+}
